df33ef50ed710a4c119d6f7136e503c31dfab7cc
unknown <you@example.com>
2024-01-12 16:10:11 +0700

    fixing Product Page

--- lib/screens/pages/product_page.dart
+++ lib/screens/pages/product_page.dart
@@ -16,6 +16,18 @@ class _ProductPageState extends State<ProductPage> {
     'assets/images/image_shoes.png',
   ];
 
+  List familiarShoes = [
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+    'assets/images/image_shoes.png',
+  ];
+
   int currenIndex = 0;
 
   @override
@@ -36,7 +48,22 @@ class _ProductPageState extends State<ProductPage> {
       );
     }
 
-    ;
+    Widget familiarShoesCard(String imageUrl) {
+      return Container(
+        width: 54,
+        height: 54,
+        margin: EdgeInsets.only(
+          right: 16,
+        ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(imageUrl),
+          ),
+          borderRadius: BorderRadius.circular(6),
+        ),
+      );
+    }
+
     Widget header() {
       int index = -1;
       return Column(
@@ -54,11 +81,11 @@ class _ProductPageState extends State<ProductPage> {
                   onTap: () {
                     Navigator.pop(context);
                   },
-                  child: Icon(
+                  child: const Icon(
                     Icons.chevron_left,
                   ),
                 ),
-                Icon(
+                const Icon(
                   Icons.shopping_bag,
                   color: AppColors.bgColor1,
                 ),
@@ -98,11 +125,226 @@ class _ProductPageState extends State<ProductPage> {
       );
     }
 
+    Widget content() {
+      int index = -1;
+
+      return Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(top: 17),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(24),
+          ),
+          color: AppColors.bgColor1,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                top: defaultMargin,
+                left: defaultMargin,
+                right: defaultMargin,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'TERREX URBAN LOW',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.primaryTextColor,
+                          ),
+                        ),
+                        Text(
+                          'Hiking',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.secondaryTextColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/button/button_wishlist.png',
+                    width: 46,
+                  ),
+                ],
+              ),
+            ),
+
+            //NOTE: PRICE
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                top: 20,
+                left: defaultMargin,
+                right: defaultMargin,
+              ),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: AppColors.bgColor2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Price starts from',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.primaryTextColor,
+                    ),
+                  ),
+                  Text(
+                    '\$143,98',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.priceColor,
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            //NOTE: Description
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                top: 30,
+                left: defaultMargin,
+                right: defaultMargin,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'Description',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.primaryTextColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Unpaved trails and mixed surfaces are easy when you have the traction and support you need. Casual enough for the daily commute.',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                      color: AppColors.subtitleTextColor,
+                    ),
+                    textAlign: TextAlign.justify,
+                  )
+                ],
+              ),
+            ),
+            //NOTE: FamiliarShoes
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(
+                top: defaultMargin,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                    child: const Text(
+                      'Fimiliar Shoes',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.primaryTextColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: familiarShoes.map(
+                        (image) {
+                          index++;
+                          return Container(
+                            margin: EdgeInsets.only(
+                                left: index == 0 ? defaultMargin : 0),
+                            child: familiarShoesCard(image),
+                          );
+                        },
+                      ).toList(),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            //NOTE: BUTTONS
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(35),
+              child: Row(
+                children: [
+                  Container(
+                    width: 54,
+                    height: 54,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/button/button_chat.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 54,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          backgroundColor: AppColors.primaryColor,
+                        ),
+                        child: Text(
+                          'Add toCart',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.primaryTextColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: AppColors.bgColor6,
       body: ListView(
         children: [
           header(),
+          content(),
         ],
       ),
     );
